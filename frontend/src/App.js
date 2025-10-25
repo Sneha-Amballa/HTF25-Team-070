b6eb742ee44e2c642cecb10b4bb76290b2fe611c
Sneha-Amballa <sneha.amballa0804@gmail.com>
2025-10-25 20:31:31 +0530

    Added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<h2 className="text-center mt-5">Dashboard (placeholder)</h2>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
